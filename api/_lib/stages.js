6f0c386917ac5d02eca1aaa7ea1d8a7a232ce449
Bruno Artacho <br.artacho@gmail.com>
2026-05-10 18:18:51 -0300

    feat(api): template padrão de etapas do processo seletivo

---
+++ api/_lib/stages.js
@@ -0,0 +1,11 @@
+export const DEFAULT_STAGES = [
+    { name: 'Enviado',                  done: false, current: true,  active: true },
+    { name: 'Triagem de CV',            done: false, current: false, active: true },
+    { name: 'Entrevista RH',            done: false, current: false, active: true },
+    { name: 'Teste Técnico',            done: false, current: false, active: true },
+    { name: 'Entrevista Técnica',       done: false, current: false, active: true },
+    { name: 'Entrevista Coordenador',   done: false, current: false, active: true },
+    { name: 'Proposta / Oferta',        done: false, current: false, active: true },
+    { name: 'Aprovado',                 done: false, current: false, active: true },
+    { name: 'Recusado',                 done: false, current: false, active: true },
+];
